c7caff05c067a386779625128205a1c849de6ec6
Simon Chiu <skhchiu@gmail.com>
2014-03-11 01:55:05 -0400

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              "nginx-tlq"
-maintainer        "Ben Dixon"
-maintainer_email  "ben@talkingquickly.co.uk"
+maintainer        "Simon Chiu"
+maintainer_email  "skhchiu@gmail.com"
 description       "Installs the nginx web server from the nginx ppa"
 version           "0.0.4"
 
